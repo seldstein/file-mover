ab59eb48cc85aa862e4de1bcddab692f6650a255
Sam Feldstein <samuelfeldstein@proton.me>
2023-12-10 10:53:05 -0600

    Update file paths

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import shutil
 import os
 
-def move_goodreads_export(/, destination_path):
+def move_goodreads_export(download_path, destination_path):
     try:
         # Check if the file exists
         if not os.path.isfile(download_path):
@@ -15,7 +15,7 @@ def move_goodreads_export(/, destination_path):
         print(f"An error occurred: {e}")
 
 # Update these paths
-download_path = '/path/to/downloaded/goodreads_export.csv'  # e.g., '/Users/yourusername/Downloads/goodreads_export.csv'
-destination_path = '/path/to/destination/folder/goodreads_export.csv'  # e.g., '/Users/yourusername/Documents/goodreads_export.csv'
+download_path = '~/Downloads/goodreads*.csv'  # e.g., '/Users/yourusername/Downloads/goodreads_export.csv'
+destination_path = '~/Documents/my_other_brain/archives/exports/goodreads*.csv'  # e.g., '/Users/yourusername/Documents/goodreads_export.csv'
 
 move_goodreads_export(download_path, destination_path)
